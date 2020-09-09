44dd892799c9436bdb54553e996e49865fb54af0
assumednormal <maxwell.w.mitchell@gmail.com>
2020-09-08 20:13:09 -0400

    using leaflet

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # nycovidtracking.com
+
+This project depends on the following data sources.
+- US county-level GeoJSON from https://eric.clst.org/tech/usgeojson/ for displaying results on a map
+- NY State county-level covid testing results from https://health.data.ny.gov/Health/New-York-State-Statewide-COVID-19-Testing/xdss-u53e
+- NY State county-level populations from https://data.ny.gov/Government-Finance/Annual-Population-Estimates-for-New-York-State-and/krt9-ym2k
---
+++ update-data-and-figures.py
@@ -0,0 +1,123 @@
+import json
+import pandas as pd
+import os
+import sys
+
+from datetime import datetime, timedelta
+from sodapy import Socrata
+
+
+print("Updating covid data")
+
+if not (token := os.environ.get("SODAPY_APPTOKEN")):
+    raise EnvironmentError("SODAPY_APPTOKEN not set")
+
+domain = "data.ny.gov"
+covid_id = "xdss-u53e"
+
+client = Socrata(domain, token)
+
+site_last_updated = 0
+if "site-last-updated" in os.listdir("."):
+    with open("site-last-updated", "r") as f:
+        site_last_updated = int(f.read())
+
+metadata = client.get_metadata(covid_id)
+if data_last_updated := metadata.get("rowsUpdatedAt"):
+    data_last_updated = int(data_last_updated)
+    if site_last_updated >= data_last_updated:
+        sys.exit(0)
+
+testing_data = client.get_all(covid_id, select="county, test_date, new_positives")
+
+
+print("Cleaning data")
+
+testing_data = [
+    {
+        "county": c["county"].lower(),
+        "test_date": datetime.strptime(c["test_date"], "%Y-%m-%dT%H:%M:%S.%f"),
+        "new_positives": int(c["new_positives"]),
+    }
+    for c in testing_data
+]
+
+with open("ny-county-populations-2019.csv", "r") as f:
+    population_data = [r.split(",") for r in f.read().strip().split("\n")][1:]
+
+population_data = [
+    {
+        "county": r[0],
+        "population": int(r[1])
+    }
+    for r in population_data
+]
+
+
+print("Joining data and calculating stats")
+
+df_testing = pd.DataFrame(testing_data)
+df_population = pd.DataFrame(population_data)
+
+df_testing = pd.merge(df_testing, df_population, on="county")
+
+total_cases = df_testing["new_positives"].sum()
+
+max_date = df_testing["test_date"].max()
+one_week_date = max_date - timedelta(days=7)
+two_week_date = max_date - timedelta(days=14)
+
+df_avg_new_daily_cases = df_testing[df_testing["test_date"] > one_week_date]
+df_avg_new_daily_cases = df_avg_new_daily_cases.groupby("county")["new_positives"].mean().reset_index()
+df_avg_new_daily_cases = df_avg_new_daily_cases.rename(columns={"new_positives": "avg_new_daily_cases"})
+
+df_avg_prior_daily_cases = df_testing[(df_testing["test_date"] > two_week_date) & (df_testing["test_date"] <= one_week_date)]
+df_avg_prior_daily_cases = df_avg_prior_daily_cases.groupby("county")["new_positives"].mean().reset_index()
+df_avg_prior_daily_cases = df_avg_prior_daily_cases.rename(columns={"new_positives": "avg_prior_daily_cases"})
+
+df_county_stats = df_testing[df_testing["test_date"] == max_date]
+df_county_stats = pd.merge(df_county_stats, df_avg_new_daily_cases, on="county")
+df_county_stats = pd.merge(df_county_stats, df_avg_prior_daily_cases, on="county")
+
+df_county_stats["avg_new_daily_cases_rate"] = df_county_stats["avg_new_daily_cases"] / df_county_stats["population"]
+df_county_stats["avg_new_daily_cases_rate_per_100k"] = df_county_stats["avg_new_daily_cases"] / df_county_stats["population"] * 100000
+
+df_county_stats["avg_prior_daily_cases_rate"] = df_county_stats["avg_prior_daily_cases"] / df_county_stats["population"]
+df_county_stats["avg_prior_daily_cases_rate_per_100k"] = df_county_stats["avg_prior_daily_cases"] / df_county_stats["population"] * 100000
+
+
+print("Joining stats with GeoJSON")
+
+with open("ny-counties-geo.json", "r") as f:
+    geojson = json.load(f)
+
+for i in range(len(geojson["features"])):
+    county = geojson["features"][i]["properties"]["NAME"].lower()
+    row = df_county_stats[df_county_stats["county"] == county]
+    geojson["features"][i]["properties"]["new_positives"] = int(row.iloc[0]["new_positives"])
+    geojson["features"][i]["properties"]["population"] = int(row.iloc[0]["population"])
+    geojson["features"][i]["properties"]["avg_new_daily_cases"] = round(row.iloc[0]["avg_new_daily_cases"], 2)
+    geojson["features"][i]["properties"]["avg_prior_daily_cases"] = round(row.iloc[0]["avg_prior_daily_cases"], 2)
+    geojson["features"][i]["properties"]["avg_new_daily_cases_rate"] = round(row.iloc[0]["avg_new_daily_cases_rate"], 2)
+    geojson["features"][i]["properties"]["avg_new_daily_cases_rate_per_100k"] = round(row.iloc[0]["avg_new_daily_cases_rate_per_100k"], 2)
+    geojson["features"][i]["properties"]["avg_prior_daily_cases_rate"] = round(row.iloc[0]["avg_prior_daily_cases_rate"], 2)
+    geojson["features"][i]["properties"]["avg_prior_daily_cases_rate_per_100k"] = round(row.iloc[0]["avg_prior_daily_cases_rate_per_100k"], 2)
+
+
+print("Saving GeoJSON")
+
+with open("ny-counties-geo.js", "w") as f:
+    f.write("var countiesData = ")
+    f.write(json.dumps(geojson))
+    f.write(";")
+
+
+print("Removing covid-data.json")
+
+os.remove("covid-data.json")
+
+
+print("Updating site-last-updated")
+
+with open("site-last-updated", "w") as f:
+    f.write(str(data_last_updated))
